ed101c725a8f82d6dc01e785f9ab3f8833e6dec5
Toni Mercadante <antonio.mercadante@ame-sp.org.br>
2023-09-10 23:16:13 -0300

    Adding query params

--- app/[lang]/[country]/page.tsx
+++ app/[lang]/[country]/page.tsx
@@ -3,7 +3,12 @@ import { Inter } from "@next/font/google";
 import styles from "./page.module.css";
 const inter = Inter({ subsets: ["latin"] });
 
-import { getLocalePartsFrom, locales, ValidLocale, getTranslator } from "@/i18n";
+import {
+  getLocalePartsFrom,
+  locales,
+  ValidLocale,
+  getTranslator,
+} from "@/i18n";
 
 export async function generateStaticParams() {
   return locales.map((locale) => getLocalePartsFrom({ locale }));
@@ -11,14 +16,17 @@ export async function generateStaticParams() {
 
 export default async function Home({
   params,
+  searchParams,
 }: {
   params: { lang: string; country: string };
+  searchParams: any; // Type your params
 }) {
   const translate = await getTranslator(
     `${params.lang}-${params.country.toUpperCase()}` as ValidLocale // our middleware ensures this is valid
   );
   return (
     <div>
+      {JSON.stringify(searchParams)}
       <h1>{translate("welcome.helloWorld")}</h1>
       <h2>
         {translate("welcome.happyYear", {
